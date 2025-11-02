5f58ace012b4e2a788edf2bc661dc37aa58d33f5
IPaulo-V <paulo.victor_sv@proton.me>
2025-11-02 10:55:21 -0300

    funçaõ de limpar

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let carrinho = [];
+let produtos = document.getElementById('lista-produtos')
+let total = document.getElementById('valor-total')
+
+function limpar () {
+
+    if (produtos != "") {
+        produtos.innerHTML = ""
+    }
+    if (total != "") {
+        total.innerHTML = ""
+    }
+  
+
+}
+function adicionar() {
+    
+}
